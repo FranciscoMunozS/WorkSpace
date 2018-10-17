acc776d67595e6290ae202e2b9a2485fac55a5da
Francisco <franciscomuniozs@gmail.com>
2018-10-17 12:02:42 -0300

    Last update - Added units and divisions

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,21 @@
 Rails.application.routes.draw do
   get 'teams/index'
+
   devise_for :users
+
   resources :workers
   resources :charges
   resources :units
   resources :divisions
   resources :teams
 
-  root 'workers#index'
+  devise_scope :user do
+    authenticated :user do
+      root 'workers#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 end
